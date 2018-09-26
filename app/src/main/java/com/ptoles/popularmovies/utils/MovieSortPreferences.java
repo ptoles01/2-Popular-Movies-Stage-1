8030a38704175b47ade05822cd9919f7ea75c565
Pat Toles <ptoles@gmail.com>
2018-09-25 22:22:42 -0400

    MainActivity better remembers its layout. Restores layoutManager to the pixel.

--- app/src/main/java/com/ptoles/popularmovies/utils/MovieSortPreferences.java
+++ app/src/main/java/com/ptoles/popularmovies/utils/MovieSortPreferences.java
@@ -8,17 +8,36 @@ import com.ptoles.popularmovies.R;
 
 public class MovieSortPreferences {
 
+    static SharedPreferences sharedPreferences;
+    static String sortOrderKey;
+    static String defaultSortOrder;
+    static String currentSortOrder;
+
+    Context context;
 
     public static String getPreferredSortOrder(Context context) {
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-
-        String sortOrderKey = context.getString(R.string.sort_order_key);
-
-        String defaultSortOrder = context.getString(R.string.pref_most_popular_key);
-
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        sortOrderKey = context.getString(R.string.sort_order_key);
+        defaultSortOrder = CONSTANTS.DEFAULT_SORT_ORDER_KEY;
         return sharedPreferences.getString(sortOrderKey, defaultSortOrder);
     }
 
+    public static void setCurrentSortOrder(Context context) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("CurrentSortOrder", currentSortOrder);
+        editor.apply();
+    }
+
+    public static void setPreferredSortOrder(Context context) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("CurrentSortOrder", defaultSortOrder);
+        editor.putString(sortOrderKey, defaultSortOrder);
+
+        editor.apply();
+    }
 
 }
